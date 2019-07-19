eb53d5317ac61fefc36cf2579631fbf22c4af312
rich <rich@nedlinux>
2019-07-19 21:15:31 +0100

    Basic untested proxy implementation. Changed around config bits to simplify things

--- config/types.go
+++ config/types.go
@@ -14,29 +14,23 @@ type TlsConfig struct {
 }
 
 type HostConfig struct {
-	Hostname string `yaml:"hostname"`
-	Upstream string `yaml:"upstream"`
+	Hostnames []string `yaml:"hostnames"`
+	Upstream  string   `yaml:"upstream"`
 
 	AllowGroups []string `yaml:"allow_groups"`
 	AllowUsers  []string `yaml:"allow_users"`
 	DenyGroups  []string `yaml:"deny_groups"`
 	DenyUsers   []string `yaml:"deny_users"`
-
-	HttpPort      int  `yaml:"http_port"`
-	HttpsPort     int  `yaml:"https_port"`
-	HttpsRedirect bool `yaml:"https_redirect"`
-
-	Ldap LdapConfig `yaml:"ldap"`
-	Tls  TlsConfig  `yaml:"tls"`
 }
 
 type Config struct {
-	HttpPort      int  `yaml:"http_port"`
-	HttpsPort     int  `yaml:"https_port"`
-	HttpsRedirect bool `yaml:"https_redirect"`
+	ListenAddress string `yaml:"listen_address"`
+	HttpPort      int    `yaml:"http_port"`
+	HttpsPort     int    `yaml:"https_port"`
+	HttpsRedirect bool   `yaml:"https_redirect"`
 
-	Ldap LdapConfig `yaml:"ldap"`
-	Tls  TlsConfig  `yaml:"tls"`
+	Ldap *LdapConfig `yaml:"ldap"`
+	Tls  *TlsConfig  `yaml:"tls"`
 
-	Hosts []HostConfig `yaml:"hosts"`
+	Hosts []*HostConfig `yaml:"hosts"`
 }
--- main.go
+++ main.go
@@ -1,29 +1,74 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/rgooding/http-ldap-auth-proxy/config"
 	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"sync"
 )
 
 var cfg *config.Config
 
-func startProxy(host config.HostConfig) error {
-	// TODO
-	return nil
+func hostForRequest(req *http.Request) (*config.HostConfig, error) {
+	reqHost := strings.ToLower(req.Host)
+	for _, host := range cfg.Hosts {
+		for _, hostname := range host.Hostnames {
+			if reqHost == strings.ToLower(hostname) {
+				return host, nil
+			}
+		}
+	}
+	return nil, errors.New("no upstream configured")
 }
 
 func main() {
 	var err error
-	cfg,err = config.Load("config.yaml")
-	if err != nil{
+	cfg, err = config.Load("config.yaml")
+	if err != nil {
 		log.Fatalf("Error loading config file: %s", err.Error())
 	}
 
-	// start proxy servers
-	for _, host := range cfg.Hosts {
-		err := startProxy(host)
+	director := func(req *http.Request) {
+		host, err := hostForRequest(req)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("ERROR: %s", err.Error())
+			return
 		}
+		upstream, err := url.Parse(host.Upstream)
+		if err != nil {
+			log.Printf("ERROR: Error parsing upstream URL %s : %s", host.Upstream, err.Error())
+			return
+		}
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", upstream.Host)
+		req.URL.Scheme = "http"
+		req.URL.Host = upstream.Host
 	}
+
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+
+	var wg sync.WaitGroup
+	if cfg.HttpPort > 0 {
+		go func() {
+			wg.Add(1)
+			log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", cfg.ListenAddress, cfg.HttpPort), nil))
+		}()
+	}
+	if cfg.HttpPort > 0 {
+		go func() {
+			wg.Add(1)
+			log.Fatal(http.ListenAndServeTLS(fmt.Sprintf("%s:%d", cfg.ListenAddress, cfg.HttpsPort),
+				cfg.Tls.Cert, cfg.Tls.Key, nil))
+		}()
+	}
+	wg.Wait()
 }
